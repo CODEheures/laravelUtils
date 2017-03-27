7fc9acad2372f45f51e5d86e7dada70e57acf21e
Sylvain <webmaster@ecortex.fr>
2017-03-27 16:25:49 +0200

    update config file

--- src/config/geoUtils.php
+++ src/config/geoUtils.php
@@ -2,8 +2,30 @@
 
 return [
 
-   'uri' => [
-       'mmdb' => 'http://geolite.maxmind.com/download/geoip/database/GeoLite2-City.mmdb.gz',
-       'md5' => 'http://geolite.maxmind.com/download/geoip/database/GeoLite2-City.md5',
+    'uri' => [
+        'mmdb' => 'http://geolite.maxmind.com/download/geoip/database/GeoLite2-City.mmdb.gz',
+        'md5' => 'http://geolite.maxmind.com/download/geoip/database/GeoLite2-City.md5',
     ],
+
+    'routes' => [
+        'prefix' => 'geoUtils',
+        'geoByIp' => [
+            'uri' => 'geoByIp',
+            'name' => 'codeheures.geoUtils.getGeoByIp'
+        ],
+        'geoLocByIp' => [
+            'uri' => 'geoLocByIp',
+            'name' => 'codeheures.geoUtils.getGeoLocByIp'
+        ],
+        'countryByIp' => [
+            'uri' => 'countryByIp',
+            'name' => 'codeheures.geoUtils.getCountryByIp'
+        ],
+        'refreshDb' => [
+            'uri' => 'refreshDb',
+            'name' => 'codeheures.geoUtils.refreshDb'
+        ]
+    ],
+
+    'ipTest' => '82.246.117.210',
 ];
