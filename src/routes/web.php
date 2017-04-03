caaad3bab75cb9d7e519ebe34cfc81cd29675fbc
Sylvain <webmaster@ecortex.fr>
2017-04-03 15:28:56 +0200

    add middlware web to routes

--- src/routes/web.php
+++ src/routes/web.php
@@ -10,7 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::group(['prefix' => config('codeheuresUtils.geoIp.routes.prefix')], function() {
+Route::group(['middleware' => 'web', 'prefix' => config('codeheuresUtils.geoIp.routes.prefix')], function() {
     Route::get('/'. config('codeheuresUtils.geoIp.routes.geoByIp.uri') .'/{ip?}', ['as' => config('codeheuresUtils.geoIp.routes.geoByIp.name'), 'uses' => '\Codeheures\LaravelUtils\Http\Controllers\UtilsController@geoByIp']);
     Route::get('/'. config('codeheuresUtils.geoIp.routes.geoLocByIp.uri') .'/{ip?}', ['as' => config('codeheuresUtils.geoIp.routes.geoLocByIp.name'), 'uses' => '\Codeheures\LaravelUtils\Http\Controllers\UtilsController@geoLocByIp']);
     Route::get('/'. config('codeheuresUtils.geoIp.routes.countryByIp.uri') .'/{ip?}', ['as' => config('codeheuresUtils.geoIp.routes.countryByIp.name'), 'uses' => '\Codeheures\LaravelUtils\Http\Controllers\UtilsController@countryByIp']);
