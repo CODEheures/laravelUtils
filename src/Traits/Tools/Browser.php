bfc3fde2969524d59f16d3ebc6315aa17f071340
CODEheures <webmaster@codeheures.fr>
2018-01-10 22:24:20 +0100

    add test to HTTP_USER_AGENT key on $_SERVER for getBrowserName function

--- src/Traits/Tools/Browser.php
+++ src/Traits/Tools/Browser.php
@@ -12,13 +12,15 @@ trait Browser
      * @return string
      */
     public static function getBrowserName() {
-        $user_agent = $_SERVER['HTTP_USER_AGENT'];
-        if (strpos($user_agent, 'Opera') || strpos($user_agent, 'OPR/')) return 'opera';
-        elseif (strpos($user_agent, 'Edge')) return 'edge';
-        elseif (strpos($user_agent, 'Chrome')) return 'chrome';
-        elseif (strpos($user_agent, 'Safari')) return 'safari';
-        elseif (strpos($user_agent, 'Firefox')) return 'firefox';
-        elseif (strpos($user_agent, 'MSIE') || strpos($user_agent, 'Trident/7')) return 'internet explorer';
+        if (array_key_exists('HTTP_USER_AGENT', $_SERVER)){
+            $user_agent = $_SERVER['HTTP_USER_AGENT'];
+            if (strpos($user_agent, 'Opera') || strpos($user_agent, 'OPR/')) return 'opera';
+            elseif (strpos($user_agent, 'Edge')) return 'edge';
+            elseif (strpos($user_agent, 'Chrome')) return 'chrome';
+            elseif (strpos($user_agent, 'Safari')) return 'safari';
+            elseif (strpos($user_agent, 'Firefox')) return 'firefox';
+            elseif (strpos($user_agent, 'MSIE') || strpos($user_agent, 'Trident/7')) return 'internet explorer';
+        }
 
         return 'Other';
     }
